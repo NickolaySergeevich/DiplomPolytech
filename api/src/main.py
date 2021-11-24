e6440a1445b5da192f9576052a0ee83d01e449c4
Николай Алексеев <asakura475@gmail.com>
2021-11-25 00:02:41 +0300

    Added docs string

--- api/src/main.py
+++ api/src/main.py
@@ -8,10 +8,22 @@ wsgi_app = application.wsgi_app
 
 @application.route("/")
 def hello() -> str:
+    """
+    Стартвоая страница (тестовая)
+
+    :return: Строка с "Привет мир"
+    """
+
     return "Hello, World! This is home api page!"
 
 
-def main():
+def main() -> None:
+    """
+    Для тестов
+
+    :return: Ничего
+    """
+
     application.run()
 
 
