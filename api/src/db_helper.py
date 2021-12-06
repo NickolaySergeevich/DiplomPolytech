ced88f1ef6157a7584d8db731ddd708970288469
Николай Алексеев <asakura475@gmail.com>
2021-12-07 00:52:33 +0300

    Успешное подключение!

--- api/src/db_helper.py
+++ api/src/db_helper.py
@@ -6,21 +6,21 @@ class DbHelper:
     Класс для работы с базой данных MySql
     """
 
-    def __init__(self, user: str, password: str, host: str, database: str):
+    def __init__(self, user: str, password: str, host: str, database_name: str):
         """
         Инициализация
 
         :param user: Имя пользователя
         :param password: Пароль пользователя
         :param host: ip-адрес базы данных
-        :param database: Название базы данных
+        :param database_name: Название базы данных
         """
 
         # Настройки подключения к бд
         self._user = user
         self._password = password
         self._host = host
-        self._database_name = database
+        self._database_name = database_name
 
         # Создание подключения
         self._connection = mysql.connector.connect(
@@ -37,6 +37,26 @@ class DbHelper:
 
         self._connection.close()
 
+    @staticmethod
+    def read_settings_file(file_name: str) -> dict:
+        """
+        Считывает настройки из файла и возвращает словарь с оными
+
+        :param file_name: Имя файла с настройками
+
+        :return: Словарь с настройками
+        """
+
+        answer = dict()
+
+        with open(file_name, 'r') as settings_file:
+            for line in settings_file:
+                options = line.split("=")
+                # Мы подразумеваем, что пользователь заполнил файл с настройками верно
+                answer[options[0]] = options[1][:-1]
+
+        return answer
+
 
 def main() -> None:
     """
@@ -45,7 +65,7 @@ def main() -> None:
     :return: Ничего
     """
 
-    pass
+    db_helper = DbHelper(**DbHelper.read_settings_file("../program_files/mysql_settings.dk"))
 
 
 if __name__ == '__main__':
